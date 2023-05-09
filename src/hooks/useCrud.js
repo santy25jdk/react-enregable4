a353dcb5b1ca59df2aa4a6a1ba41972bccdcfa6b
santy25jdk <santyquispevillegas@gmail.com>
2023-05-09 19:47:09 -0300

    first commit

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -1,54 +1,51 @@
-import axios from "axios"
-import { useState } from "react"
+import axios from "axios";
+import { useState } from "react";
 
 const useCrud = () => {
-
-  const [users, setUsers] = useState()
-
+  const [users, setUsers] = useState();
 
   const getALlUsers = () => {
-    const URL = 'https://users-crud.academlo.tech/users/'
-    axios.get(URL)
-    .then(res => setUsers(res.data))
-    .catch(err => console.log(err))
-  }
-  
-  const createNewUsers = data => {
-    const URL = 'https://users-crud.academlo.tech/users/'
-    axios.post(URL, data)
-    .then(res => {
-      console.log(res.data)
-      getALlUsers()
-    })
-    .catch(err => console.log(err))
-  }
+    const URL = "https://crud-user-xah7.onrender.com/users/";
+    axios
+      .get(URL)
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  };
 
-  const deleteUserById = id => {
-    const URL = `https://users-crud.academlo.tech/users/${id}/`
-    axios.delete(URL)
-    .then(res => {
-        console.log(res.data)
-        getALlUsers()
-    })
-    .catch(err => console.log(err))
-  }
+  const createNewUsers = (data) => {
+    const URL = "https://crud-user-xah7.onrender.com/users/";
+    axios
+      .post(URL, data)
+      .then((res) => {
+        console.log(res.data);
+        getALlUsers();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const deleteUserById = (id) => {
+    const URL = `https://crud-user-xah7.onrender.com/users/${id}/`;
+    axios
+      .delete(URL)
+      .then((res) => {
+        console.log(res.data);
+        getALlUsers();
+      })
+      .catch((err) => console.log(err));
+  };
 
   const updateUserById = (id, data) => {
-    const URL = `https://users-crud.academlo.tech/users/${id}/`
-    axios.put(URL, data)
-    .then(res => {
-      console.log(res.data)
-      getALlUsers()
-    })
-    .catch(err => console.log(err))
-  }
+    const URL = `https://crud-user-xah7.onrender.com/${id}/`;
+    axios
+      .put(URL, data)
+      .then((res) => {
+        console.log(res.data);
+        getALlUsers();
+      })
+      .catch((err) => console.log(err));
+  };
 
-  return {users,
-    getALlUsers,
-    createNewUsers,
-    deleteUserById,
-    updateUserById
-  }
-}
+  return { users, getALlUsers, createNewUsers, deleteUserById, updateUserById };
+};
 
-export default useCrud
+export default useCrud;
